1db16c80a3b2e6a2eb2adc0c55a544240f5c0894
fexd12 <you@example.com>
2019-12-16 17:21:39 -0300

    commit

---
+++ dia1/calculadora.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import path from 'path';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+router.post('/', (req, res) => {
+    let operando1 = req.body.operando1;
+    let operando2 = req.body.operando2;
+    let operador = req.body.operador;
+
+    let resultado;
+
+    if (operador == '+') resultado = operando1 + operando2;
+    else if (operador == '-') resultado = operando1 - operando2;
+    else if (operador == '*') resultado = operando1 * operando2;
+    else if (operador == '/') resultado = operando1 / operando2;
+
+    res.status = 200;
+    //res.send(JSON.stringify({ resultado }));
+    res.send(resultado);
+
+});
+export default router;
